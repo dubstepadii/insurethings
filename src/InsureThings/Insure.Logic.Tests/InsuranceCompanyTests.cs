c2cbfa7dae7fd4e9338950457590a29a343482bb
dubstepadii <arturs.bucenieks@gmail.com>
2019-04-10 20:33:15 +0300

    Adding test for AddRisk and RemoveRisk methods - handling exceptions

--- src/InsureThings/Insure.Logic.Tests/InsuranceCompanyTests.cs
+++ src/InsureThings/Insure.Logic.Tests/InsuranceCompanyTests.cs
@@ -15,10 +15,59 @@ namespace Insure.Logic.Tests
         }
 
         [TestMethod]
-        public void AddRisk_NameOfInsuredObectNull_ThrowsException()
+        public void AddRisk_NameOfInsuredObjectNull_ThrowsException()
         {
             Action result = () => _target.AddRisk(null, new Risk(), DateTime.Now, DateTime.Now);
             Assert.ThrowsException<ArgumentNullException>(result);
+        }
+
+        [TestMethod]
+        public void AddRisk_NameOfInsuredObjectEmpty_ThrowsException()
+        {
+            Action result = () => _target.AddRisk(string.Empty, new Risk(), DateTime.Now, DateTime.Now);
+            Assert.ThrowsException<ArgumentNullException>(result);
+        }
+
+        [TestMethod]
+        public void AddRisk_NameOfInsuredObjectWhiteSpace_ThrowsException()
+        {
+            Action result = () => _target.AddRisk("", new Risk(), DateTime.Now, DateTime.Now);
+            Assert.ThrowsException<ArgumentNullException>(result);
+        }
+
+        [TestMethod]
+        public void AddRisk_ValidFromInPast_ThrowsException()
+        {
+            Action result = () => _target.AddRisk("object", new Risk(), DateTime.Now.AddDays(-2), DateTime.Now);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(result);
+        }
+
+        [TestMethod]
+        public void RemoveRisk_NameOfInsuredObjectNull_ThrowsException()
+        {
+            Action result = () => _target.RemoveRisk(null, new Risk(), DateTime.Now, DateTime.Now);
+            Assert.ThrowsException<ArgumentNullException>(result);
+        }
+
+        [TestMethod]
+        public void RemoveRisk_NameOfInsuredObjectEmpty_ThrowsException()
+        {
+            Action result = () => _target.RemoveRisk(string.Empty, new Risk(), DateTime.Now, DateTime.Now);
+            Assert.ThrowsException<ArgumentNullException>(result);
+        }
+
+        [TestMethod]
+        public void RemoveRisk_NameOfInsuredObjectWhiteSpace_ThrowsException()
+        {
+            Action result = () => _target.RemoveRisk("", new Risk(), DateTime.Now, DateTime.Now);
+            Assert.ThrowsException<ArgumentNullException>(result);
+        }
+
+        [TestMethod]
+        public void RemoveRisk_ValidTillOutOfRange_ThrowsException()
+        {
+            Action result = () => _target.RemoveRisk("object", new Risk(), DateTime.Now.AddDays(5), DateTime.Now);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(result);
         }
     }
 }
